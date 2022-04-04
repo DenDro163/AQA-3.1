52393c3bb09bf7cf7b1bc2678f5eda51f6adb09e
Andrey Denisov <bk6om1986@gmail.com>
2022-04-04 12:53:09 +0400

    Third commit

--- src/test/java/ru/netology/web/CallBackWebDriverManagerTest.java
+++ src/test/java/ru/netology/web/CallBackWebDriverManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -39,11 +40,77 @@ public class CallBackWebDriverManagerTest {
     }
 
     @Test
-    public void shouldSent() {
-        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Иван Бакланов");
-        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
-        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
-        driver.findElement(cssSelector(".form-field .button__content")).click();
-        assertTrue(driver.findElement(cssSelector("[data-test-id=order-success]")).isEnabled());
+    public void shouldTestSendForm() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Бакланов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();//кнопка продолжить
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=order-success]")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfFormEmpty() {//
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfNameFieldEmpty() {//
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfTelephoneFieldEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василь Протонов");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfCheckboxFieldEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василь Протонов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfBadName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Lol Shto");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfBadTelephoneLessNumber() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василь Протонов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+12345");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfBadTelephoneMoreNumber() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василь Протонов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+723456789012");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldTestWarningIfBadTelephoneLetterNumber() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василь Протонов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+afornfgcjsq");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(By.cssSelector(".input_invalid")).isEnabled());
     }
 }
